2a10ba35f897e86536b493eb1e335df99090961e
tarataichenkodmitri <tarataichenko@gmail.com>
2021-11-03 11:30:13 +0600

    hello wrold with servlet

---
+++ src/main/java/com/hornhub/hornsite/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.hornhub.hornsite;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+        System.out.println("Servlet is being destroyed");
+    }
+}
